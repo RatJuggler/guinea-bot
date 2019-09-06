23f4662f8b9a548ec1c467d2c0d5190c7a8fd10b
John Chase <ratteal@gmail.com>
2019-09-06 20:37:02 +0100

    Log some messages on startup.

--- guineabot/__main__.py
+++ guineabot/__main__.py
@@ -8,19 +8,22 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 
 def build_guinea_pig_machine():
-    m = StateMachine(15, 2000)
-    m.add_state("SLEEPING", GuineaPigState, [-20, 3, 1])
-    m.add_state("AWAKE", GuineaPigState, [5, 5, 2])
-    m.add_state("THINKING", GuineaPigState, [1, 3, 1])
-    m.add_state("EATING", GuineaPigState, [5, -10, 4])
-    m.add_state("DRINKING", GuineaPigState, [5, 5, -80])
-    m.add_state("WANDERING", GuineaPigState, [10, 10, 5])
-    return m
+    sm = StateMachine(15, 2000)
+    sm.add_state("SLEEPING", GuineaPigState, [-20, 3, 1])
+    sm.add_state("AWAKE", GuineaPigState, [5, 5, 2])
+    sm.add_state("THINKING", GuineaPigState, [1, 3, 1])
+    sm.add_state("EATING", GuineaPigState, [5, -10, 4])
+    sm.add_state("DRINKING", GuineaPigState, [5, 5, -80])
+    sm.add_state("WANDERING", GuineaPigState, [10, 10, 5])
+    return sm
 
 
 def simulate_guinea_pig():
+    logging.info("Booting guinea pig...")
     gp_machine = build_guinea_pig_machine()
-    gp_machine.run(GuineaPig("SLEEPING", 20, 10, 10))
+    a_guinea_pig = GuineaPig("SLEEPING", 20, 10, 10)
+    logging.info("It's alive!")
+    gp_machine.run(a_guinea_pig)
     gp_machine.stats()
 
 
