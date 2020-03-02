58df024ec2b9e9a1ff9734849fe852a0a83d325d
John Chase <ratteal@gmail.com>
2020-03-02 12:46:39 +0000

    Make class attributes private and introduce a generic for the state change data object.

--- guineabot/statemachine.py
+++ guineabot/statemachine.py
@@ -1,28 +1,30 @@
 import logging
 
 from time import sleep
-from typing import Callable, List
+from typing import Callable, List, Generic, TypeVar
+
+T = TypeVar('T')
 
 
 class State:
 
-    def transition(self, data) -> str:
+    def transition(self, data: Generic[T]) -> str:
         pass
 
 
 class StateMachine:
 
     def __init__(self, interval: int, days: int) -> None:
-        self.interval = interval
-        self.days = days
-        self.run_time = (self.days * 24 * 60) // self.interval
-        self.states = {}
-        self.counts = {}
+        self.__interval = interval
+        self.__days = days
+        self.__run_time = (self.__days * 24 * 60) // self.__interval
+        self.__states = {}
+        self.__counts = {}
 
     def add_state(self, name: str, handler: Callable, changes: List[int]) -> None:
         name = name.upper()
-        self.states[name] = handler(name, changes)
-        self.counts[name] = 0
+        self.__states[name] = handler(name, changes)
+        self.__counts[name] = 0
 
     @staticmethod
     def __format_time(total_minutes: int) -> str:
@@ -31,23 +33,23 @@ class StateMachine:
         return "{0:02d}:{1:02d}".format(hours, minutes)
 
     def __format_days_time(self, ticks: int) -> str:
-        total_minutes = ticks * self.interval
+        total_minutes = ticks * self.__interval
         days = total_minutes // (24 * 60)
         total_minutes -= days * 24 * 60
         return "Day: {0:4,d} - {1}".format(days, self.__format_time(total_minutes))
 
-    def run(self, start_state: str, data) -> None:
+    def run(self, start_state: str, data: Generic[T]) -> None:
         new_state = start_state
-        for i in range(self.run_time):
+        for i in range(self.__run_time):
             logging.info("{0} >> {1}".format(self.__format_days_time(i), str(data)))
-            state = self.states[new_state]
-            self.counts[new_state] += 1
+            state = self.__states[new_state]
+            self.__counts[new_state] += 1
             new_state = state.transition(data)
-            sleep(self.interval * 60)
+            sleep(self.__interval * 60)
 
     def stats(self) -> None:
         logging.info("\n\n\nState     : Time spent in state (% and daily avg.)")
-        for state in self.counts:
-            percentage = self.counts[state] / self.run_time * 100
-            average = self.__format_time(self.counts[state] * self.interval // self.days)
+        for state in self.__counts:
+            percentage = self.__counts[state] / self.__run_time * 100
+            average = self.__format_time(self.__counts[state] * self.__interval // self.__days)
             logging.info("{0:9} : {1:04.2f}% - {2}".format(state, percentage, average))
