217d32e3f8f2f5aa7e2aea00cb945c3b1cd48c6d
John Chase <ratteal@gmail.com>
2020-03-02 18:38:04 +0000

    Fill out some documentation and improve naming.

--- guineabot/statemachine.py
+++ guineabot/statemachine.py
@@ -7,39 +7,80 @@ T = TypeVar('T')
 
 
 class State:
+    """
+    States are used to configure the StateMachine.
+    """
 
     def transition(self, data: Generic[T]) -> str:
+        """
+        A transition to the next state.
+        :param data: Object which informs the transition on the next state.
+        :return: The next state (not necessarily different to the current state)
+        """
         pass
 
 
 class StateMachine:
+    """
+    A pseudo real-time state machine which runs for a fixed duration of days and transitions to the next state after each fixed
+    interval of minutes.
+    """
 
-    def __init__(self, interval: int, days: int, accelerated: bool) -> None:
+    def __init__(self, interval: int, duration: int, accelerated: bool) -> None:
+        """
+        Initialise a StateMachine instance.
+        :param interval: The time interval between state transitions in minutes
+        :param duration: How long the state machine will run for in days
+        :param accelerated: Don't wait for the time interval
+        """
         self.__interval = interval
-        self.__days = days
+        self.__duration = duration
         self.__accelerated = accelerated
-        self.__run_time = (self.__days * 24 * 60) // self.__interval
+        self.__run_time = (self.__duration * 24 * 60) // self.__interval
         self.__states = {}
         self.__counts = {}
 
     def add_state(self, name: str, handler: Callable, changes: List[int]) -> None:
+        """
+        Add a new state to this machine instance.
+        :param name: State name
+        :param handler: State handler
+        :param changes:
+        :return:
+        """
         name = name.upper()
         self.__states[name] = handler(name, changes)
         self.__counts[name] = 0
 
     @staticmethod
     def __format_time(total_minutes: int) -> str:
+        """
+        Format total minutes
+        :param total_minutes: to format
+        :return: String of total minutes in HH:MM format
+        """
         hours = total_minutes // 60
         minutes = total_minutes - (hours * 60)
         return "{0:02d}:{1:02d}".format(hours, minutes)
 
     def __format_days_time(self, ticks: int) -> str:
+        """
+        Format ticks into days, hours and minutes
+        :param ticks:
+        :return: String of ticks in DD - HH:MM format
+        """
         total_minutes = ticks * self.__interval
         days = total_minutes // (24 * 60)
         total_minutes -= days * 24 * 60
         return "Day: {0:4,d} - {1}".format(days, self.__format_time(total_minutes))
 
     def run(self, start_state: str, data: Generic[T]) -> None:
+        """
+        Run the state machine.
+        :param start_state: Initial state
+        :param data: Object which informs the state transition on the next state.
+        :return: No meaningful return
+        """
         new_state = start_state
         for i in range(self.__run_time):
             logging.debug("{0} >> {1}".format(self.__format_days_time(i), str(data)))
@@ -50,8 +91,12 @@ class StateMachine:
                 sleep(self.__interval * 60)
 
     def stats(self) -> None:
+        """
+        Log stats on time spent in each state.
+        :return: No meaningful return
+        """
         logging.info("Dumping stats...\n                                 State     : Time spent in state (% and daily avg.)")
         for state in self.__counts:
             percentage = self.__counts[state] / self.__run_time * 100
-            average = self.__format_time(self.__counts[state] * self.__interval // self.__days)
+            average = self.__format_time(self.__counts[state] * self.__interval // self.__duration)
             logging.info("{0:9} : {1:04.2f}% - {2}".format(state, percentage, average))
