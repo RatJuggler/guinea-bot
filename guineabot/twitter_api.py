29c72846db109f789babc5d26426f50560e28709
John Chase <ratteal@gmail.com>
2020-03-04 15:42:24 +0000

    Only mute if not already muted. Add documentation.

--- guineabot/twitter_api.py
+++ guineabot/twitter_api.py
@@ -10,24 +10,64 @@ from .smt_logging import smt_logger
 
 
 class TwitterService:
+    """
+    Interface to define the function signatures for interacting with Twitter.
+    """
 
     def tweet(self, message: str) -> None:
+        """
+        Tweet the supplied message.
+        :param message: String to tweet
+        :return: No meaningful return
+        """
         pass
 
     def tweet_with_photo(self, message: str, photo_path: str) -> None:
+        """
+        Tweet the supplied message and photo.
+        :param message: String to tweet
+        :param photo_path: Path to photo to tweet
+        :return: No meaningful return
+        """
         pass
 
     def get_current_friends(self) -> List[int]:
+        """
+        Get a list of the current friends.
+        :return: List of friend ids
+        """
         pass
 
     def find_new_friend(self, friends: List[int]) -> List[int]:
+        """
+        Find and follow a new friend who passes the friendship test.
+        :param friends: Current list of friend ids
+        :return: Updated list of friend ids
+        """
         pass
 
     def prune_friends(self, friends: List[int]) -> List[int]:
+        """
+        Remove friends who no longer pass the friendship test, unless they are following us, in which case mute them.
+        :param friends: Current list of friend ids
+        :return: Updated list of friend ids
+        """
+        pass
+
+    def unmute_all(self) -> None:
+        """
+        Maintenance function to remove all mutes from friends.
+        :return: No meaningful return
+        """
         pass
 
 
 def get_twitter_service(quiet: bool) -> TwitterService:
+    """
+    Factory to return a Twitter service instance.
+    :param quiet: If true return a dummy version of the service
+    :return: Returns a TwitterService
+    """
     if quiet:
         return TwitterServiceQuiet()
     else:
@@ -35,6 +75,10 @@ def get_twitter_service(quiet: bool) -> TwitterService:
 
 
 class TwitterServiceQuiet(TwitterService):
+    """
+    Quiet version of the TwitterService.
+    Logs what the live service would do without actually invoking the Twitter API.
+    """
 
     def __init__(self) -> None:
         smt_logger.info("Quiet Mode On!")
@@ -56,8 +100,15 @@ class TwitterServiceQuiet(TwitterService):
         smt_logger.info("Would have tried to pruned friends!")
         return []
 
+    def unmute_all(self) -> None:
+        smt_logger.info("Would have un-muted all muted friends!")
+
 
 class TwitterServiceLive(TwitterService):
+    """
+    Live version of the TwitterService.
+    Invokes the Twitter API using the Tweepy module.
+    """
 
     def __init__(self) -> None:
         self.__consumer_key = os.getenv("TWITTER_CONSUMER_KEY")
@@ -116,7 +167,9 @@ class TwitterServiceLive(TwitterService):
         while page_no < 100:
             page = self.__api.search_users("guinea pig", 20, page_no)
             for new_friend in page:
-                if new_friend.id not in friends and self.__friendship_test(new_friend.name, new_friend.screen_name):
+                if not new_friend.follow_request_sent and \
+                        new_friend.id not in friends and \
+                        self.__friendship_test(new_friend.name, new_friend.screen_name):
                     new_friend.follow()
                     friends.append(new_friend.id)
                     self.tweet("I've decided to follow {0}.".format(new_friend.name))
@@ -126,12 +179,19 @@ class TwitterServiceLive(TwitterService):
         return friends
 
     def prune_friends(self, friends: List[int]) -> List[int]:
+        muted = self.__api.mutes_ids()
         for friendship in self.__api.lookup_friendships(friends):
             if not self.__friendship_test(friendship.name, friendship.screen_name):
                 if friendship.is_followed_by:
-                    self.__api.create_mute(friendship.id)
-                    smt_logger.info("Muted: {0} - {1}".format(friendship.name, friendship.screen_name))
+                    if friendship.id not in muted:
+                        self.__api.create_mute(friendship.id)
+                        smt_logger.info("Muted: {0} - {1}".format(friendship.name, friendship.screen_name))
                 else:
                     self.__api.destroy_friendship(friendship.id)
                     smt_logger.info("Un-followed: {0} - {1}".format(friendship.name, friendship.screen_name))
         return self.get_current_friends()
+
+    def unmute_all(self) -> None:
+        muted = self.__api.mutes_ids()
+        for mute in muted:
+            self.__api.destroy_mute(mute)
