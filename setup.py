a8faca5da4116ec5cc0c25007b76eefd3770781a
John Chase <ratteal@gmail.com>
2020-03-11 16:48:20 +0000

    Bump version number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='guineabot',
-    version='0.0.5',
+    version='0.0.6',
     description='A guinea pig Twitter bot.',
     long_description=long_description,
     long_description_content_type='text/markdown',
