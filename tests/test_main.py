560fef0b7cd458d2eb0a52ee77712668bc0d7257
John Chase <ratteal@gmail.com>
2020-03-21 19:41:22 +0000

    Add more tests on main.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,20 @@
 from unittest import TestCase
+from unittest.mock import patch, MagicMock
+
 from click.testing import CliRunner
+from testfixtures import LogCapture
 
 import guineabot.__main__ as main
+import guineabot.age_logging as al
+
+
+def _init_log_check(log_out: LogCapture, expected1: str, expected2: str, expected3: str, expected4: str) -> None:
+    root = 'root'
+    log_level = al.logging.getLevelName(al.logging.INFO)
+    log_out.check_present((root, log_level, expected1),
+                          (root, log_level, expected2),
+                          (root, log_level, expected3),
+                          (root, log_level, expected4))
 
 
 class TestMain(TestCase):
@@ -27,3 +40,32 @@ class TestMain(TestCase):
         result = self.runner.invoke(main.simulate_guinea_pig, ['--version'])
         self.assertEqual(result.exit_code, 0)
         self.assertIn("simulate-guinea-pig, version ", result.output)
+
+    def test_invalid_name(self) -> None:
+        result = self.runner.invoke(main.simulate_guinea_pig, ['--name', 'AVeryLongForAGuineaPig'])
+        self.assertEqual(result.exit_code, 2)
+        self.assertIn('Error: Invalid value for \'-n\' / \'--name\': AVeryLongForAGuineaPig', result.output)
+
+    def test_invalid_photos_folder(self) -> None:
+        result = self.runner.invoke(main.simulate_guinea_pig, ['--photos-folder', '/path/to/nowhere'])
+        self.assertEqual(result.exit_code, 2)
+        self.assertIn('Error: Invalid value for \'-p\' / \'--photos-folder\': /path/to/nowhere', result.output)
+
+    @patch('guineabot.__main__.AgeTracker')
+    @patch('guineabot.__main__.create_guinea_pig')
+    @patch('guineabot.__main__.build_guinea_pig_machine')
+    def test_duration(self,
+                      build_guinea_pig_machine_mock: MagicMock,
+                      create_guinea_pig_mock: MagicMock,
+                      age_tracker_mock: MagicMock) -> None:
+        with LogCapture(level=al.logging.INFO) as log_out:
+            result = self.runner.invoke(main.simulate_guinea_pig, ['-d', '99'])
+        self.assertEqual(result.exit_code, 0)
+        _init_log_check(log_out,
+                        'INITIALISE >> Booting guinea pig Holly...',
+                        'INITIALISE >> Bot duration (guinea pig lifespan): 99 days',
+                        'INITIALISE >> State interval (changes in guinea pig activity): 15 minutes',
+                        'INITIALISE >> It\'s alive!')
+        age_tracker_mock.assert_called_once()
+        create_guinea_pig_mock.assert_called_once()
+        build_guinea_pig_machine_mock.assert_called_once()
