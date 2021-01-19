eca071e355250f170507a9a307c891d88dcd38dd
John Chase <ratteal@gmail.com>
2021-01-19 11:46:59 +0000

    Clearer naming.

--- tests/test_guineapig.py
+++ tests/test_guineapig.py
@@ -10,33 +10,33 @@ class TestGuineaPig(TestCase):
         pass
 
     def test_initial_str(self) -> None:
-        gp = GuineaPig("Test", 9, 0, "SLEEPING", 20, 10, 10, Mock())
-        self.assertEqual("GuineaPig:(Name: Test, Lifespan: 9 days, Current Age: 0 mins, State: SLEEPING, "
+        gp = GuineaPig("TestGP", 9, 0, "SLEEPING", 20, 10, 10, Mock())
+        self.assertEqual("GuineaPig:(Name: TestGP, Lifespan: 9 days, Current Age: 0 mins, State: SLEEPING, "
                          "Tired: 20, Hunger: 10, Thirst: 10)", gp.__str__())
 
     def test_single_update(self) -> None:
-        gp = GuineaPig("Test", 9, 0, "SLEEPING", 20, 10, 10, Mock())
+        gp = GuineaPig("TestGP", 9, 0, "SLEEPING", 20, 10, 10, Mock())
         self.assertFalse(gp.has_died())
-        gp.update("TEST", [1, 2, 3], 1)
+        gp.update("TEST_STATE", [1, 2, 3], 1)
         self.assertFalse(gp.has_died())
-        self.assertEqual("GuineaPig:(Name: Test, Lifespan: 9 days, Current Age: 1 mins, State: TEST, "
+        self.assertEqual("GuineaPig:(Name: TestGP, Lifespan: 9 days, Current Age: 1 mins, State: TEST_STATE, "
                          "Tired: 21, Hunger: 12, Thirst: 13)", gp.__str__())
 
     def test_multiple_updates(self) -> None:
-        gp = GuineaPig("Test", 9, 0, "SLEEPING", 20, 10, 10, Mock())
+        gp = GuineaPig("TestGP", 9, 0, "SLEEPING", 20, 10, 10, Mock())
         for i in range(27):
             self.assertFalse(gp.has_died())
-            gp.update("TEST", [1, 2, 3], 1)
+            gp.update("TEST_STATE", [1, 2, 3], 1)
         self.assertFalse(gp.has_died())
-        self.assertEqual("GuineaPig:(Name: Test, Lifespan: 9 days, Current Age: 27 mins, State: TEST, "
+        self.assertEqual("GuineaPig:(Name: TestGP, Lifespan: 9 days, Current Age: 27 mins, State: TEST_STATE, "
                          "Tired: 47, Hunger: 64, Thirst: 91)", gp.__str__())
 
     def test_can_die(self) -> None:
         lifespan = 4
-        gp = GuineaPig("Test", lifespan, 0, "SLEEPING", 20, 10, 10, Mock())
+        gp = GuineaPig("TestGP", lifespan, 0, "SLEEPING", 20, 10, 10, Mock())
         for i in range(lifespan * 24 * 60):
             self.assertFalse(gp.has_died())
-            gp.update("TEST", [0, 0, 0], 1)
+            gp.update("TEST_STATE", [0, 0, 0], 1)
         self.assertTrue(gp.has_died())
-        self.assertEqual("GuineaPig:(Name: Test, Lifespan: 4 days, Current Age: 5760 mins, State: TEST, "
+        self.assertEqual("GuineaPig:(Name: TestGP, Lifespan: 4 days, Current Age: 5760 mins, State: TEST_STATE, "
                          "Tired: 20, Hunger: 10, Thirst: 10)", gp.__str__())
