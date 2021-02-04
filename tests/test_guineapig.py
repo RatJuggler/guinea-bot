8b42540d71286b88d8a2fe7b56156c0226242c05
John Chase <ratteal@gmail.com>
2021-02-04 17:46:36 +0000

    Add save file to tests.

--- tests/test_guineapig.py
+++ tests/test_guineapig.py
@@ -1,42 +1,45 @@
+import os
+
 from unittest import TestCase
 from unittest.mock import Mock
 
-from guineabot.guineapig import GuineaPig
+from guineabot.guineapig import GuineaPig, build_save_filename
 
 
 class TestGuineaPig(TestCase):
 
     def setUp(self) -> None:
-        pass
+        self.__name = "TestGP"
+        self.__save_file = build_save_filename(os.environ.get('HOME', ''), self.__name)
 
     def test_initial_str(self) -> None:
-        gp = GuineaPig("TestGP", 9, 0, "SLEEPING", 20, 10, 10, Mock())
-        self.assertEqual("GuineaPig:(Name: TestGP, Lifespan: 9 days, Current Age: 0 mins, State: SLEEPING, "
-                         "Tired: 20, Hunger: 10, Thirst: 10)", gp.__str__())
+        gp = GuineaPig(self.__name, self.__save_file, 9, 0, "SLEEPING", 20, 10, 10, Mock())
+        self.assertEqual("GuineaPig:(Name: {0}, Save File: {1}, Lifespan: 9 days, Current Age: 0 mins, State: SLEEPING, "
+                         "Tired: 20, Hunger: 10, Thirst: 10)".format(self.__name, self.__save_file), gp.__str__())
 
     def test_single_update(self) -> None:
-        gp = GuineaPig("TestGP", 9, 0, "SLEEPING", 20, 10, 10, Mock())
+        gp = GuineaPig(self.__name, self.__save_file, 9, 0, "SLEEPING", 20, 10, 10, Mock())
         self.assertFalse(gp.has_died())
         gp.update("TEST_STATE", [1, 2, 3], 1)
         self.assertFalse(gp.has_died())
-        self.assertEqual("GuineaPig:(Name: TestGP, Lifespan: 9 days, Current Age: 1 mins, State: TEST_STATE, "
-                         "Tired: 21, Hunger: 12, Thirst: 13)", gp.__str__())
+        self.assertEqual("GuineaPig:(Name: {0}, Save File: {1}, Lifespan: 9 days, Current Age: 1 mins, State: TEST_STATE, "
+                         "Tired: 21, Hunger: 12, Thirst: 13)".format(self.__name, self.__save_file), gp.__str__())
 
     def test_multiple_updates(self) -> None:
-        gp = GuineaPig("TestGP", 9, 0, "SLEEPING", 20, 10, 10, Mock())
+        gp = GuineaPig(self.__name, self.__save_file, 9, 0, "SLEEPING", 20, 10, 10, Mock())
         for i in range(27):
             self.assertFalse(gp.has_died())
             gp.update("TEST_STATE", [1, 2, 3], 1)
         self.assertFalse(gp.has_died())
-        self.assertEqual("GuineaPig:(Name: TestGP, Lifespan: 9 days, Current Age: 27 mins, State: TEST_STATE, "
-                         "Tired: 47, Hunger: 64, Thirst: 91)", gp.__str__())
+        self.assertEqual("GuineaPig:(Name: {0}, Save File: {1}, Lifespan: 9 days, Current Age: 27 mins, State: TEST_STATE, "
+                         "Tired: 47, Hunger: 64, Thirst: 91)".format(self.__name, self.__save_file), gp.__str__())
 
     def test_can_die(self) -> None:
         lifespan = 4
-        gp = GuineaPig("TestGP", lifespan, 0, "SLEEPING", 20, 10, 10, Mock())
+        gp = GuineaPig(self.__name, self.__save_file, lifespan, 0, "SLEEPING", 20, 10, 10, Mock())
         for i in range(lifespan * 24 * 60):
             self.assertFalse(gp.has_died())
             gp.update("TEST_STATE", [0, 0, 0], 1)
         self.assertTrue(gp.has_died())
-        self.assertEqual("GuineaPig:(Name: TestGP, Lifespan: 4 days, Current Age: 5760 mins, State: TEST_STATE, "
-                         "Tired: 20, Hunger: 10, Thirst: 10)", gp.__str__())
+        self.assertEqual("GuineaPig:(Name: {0}, Save File: {1}, Lifespan: 4 days, Current Age: 5760 mins, State: TEST_STATE, "
+                         "Tired: 20, Hunger: 10, Thirst: 10)".format(self.__name, self.__save_file), gp.__str__())
--- tests/test_guineapig_state.py
+++ tests/test_guineapig_state.py
@@ -1,18 +1,21 @@
+import os
+
 from unittest import TestCase
 from unittest.mock import Mock
 
-from guineabot.guineapig import GuineaPig
+from guineabot.guineapig import GuineaPig, build_save_filename
 from guineabot.guineapig_states import GuineaPigState
 
 
 class TestGuineaPigState(TestCase):
 
     def setUp(self) -> None:
-        pass
+        self.__name = "TestGP"
+        self.__save_file = build_save_filename(os.environ.get('HOME', ''), self.__name)
 
     def test_state_transition(self) -> None:
-        gp = GuineaPig("TestGP", 9, 0, "SLEEPING", 80, 10, 10, Mock())
+        gp = GuineaPig(self.__name, self.__save_file, 9, 0, "SLEEPING", 80, 10, 10, Mock())
         gps = GuineaPigState("TEST_STATE", [1, 2, 3])
         self.assertEqual("SLEEPING", gps.transition(1, gp))
-        self.assertEqual("GuineaPig:(Name: TestGP, Lifespan: 9 days, Current Age: 1 mins, State: TEST_STATE, "
-                         "Tired: 81, Hunger: 12, Thirst: 13)", gp.__str__())
+        self.assertEqual("GuineaPig:(Name: {0}, Save File: {1}, Lifespan: 9 days, Current Age: 1 mins, State: TEST_STATE, "
+                         "Tired: 81, Hunger: 12, Thirst: 13)".format(self.__name, self.__save_file), gp.__str__())
