3ea12124969b2a7d6821df80908e399f0a2efae2
John Chase <ratteal@gmail.com>
2021-02-04 23:04:21 +0000

    Simplify import.

--- tests/utils.py
+++ tests/utils.py
@@ -1,10 +1,10 @@
 from testfixtures import LogCapture
 
-import guineabot.age_logging as al
+from guineabot.age_logging import logging
 
 
 def log_check(log_out: LogCapture, *expects: str) -> None:
     root = "root"
-    log_level = al.logging.getLevelName(al.logging.INFO)
+    log_level = logging.getLevelName(logging.INFO)
     for expected in expects:
         log_out.check_present((root, log_level, expected))
