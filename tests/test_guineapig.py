b05b32e0c86eddc044c279d25e9c1e30d0c758c8
John Chase <ratteal@gmail.com>
2021-02-04 19:15:41 +0000

    Simplify final state check.

--- tests/test_guineapig.py
+++ tests/test_guineapig.py
@@ -15,18 +15,21 @@ class TestGuineaPig(TestCase):
     def build_guinea_pig(self, lifespan: int = 9) -> GuineaPig:
         return GuineaPig(self.__name, self.__save_file, lifespan, 0, "SLEEPING", 20, 10, 10, Mock())
 
+    def check_state(self, gp: GuineaPig, lifespan: int, age: int, state: str, tired: int, hunger: int, thirst: int) -> None:
+        self.assertEqual("GuineaPig:(Name: {0}, Save File: {1}, Lifespan: {2} days, Current Age: {3} mins, State: {4}, "
+                         "Tired: {5}, Hunger: {6}, Thirst: {7})"
+                         .format(self.__name, self.__save_file, lifespan, age, state, tired, hunger, thirst), gp.__str__())
+
     def test_initial_str(self) -> None:
         gp = self.build_guinea_pig()
-        self.assertEqual("GuineaPig:(Name: {0}, Save File: {1}, Lifespan: 9 days, Current Age: 0 mins, State: SLEEPING, "
-                         "Tired: 20, Hunger: 10, Thirst: 10)".format(self.__name, self.__save_file), gp.__str__())
+        self.check_state(gp, 9, 0, "SLEEPING", 20, 10, 10)
 
     def test_single_update(self) -> None:
         gp = self.build_guinea_pig()
         self.assertFalse(gp.has_died())
         gp.update("TEST_STATE", [1, 2, 3], 1)
         self.assertFalse(gp.has_died())
-        self.assertEqual("GuineaPig:(Name: {0}, Save File: {1}, Lifespan: 9 days, Current Age: 1 mins, State: TEST_STATE, "
-                         "Tired: 21, Hunger: 12, Thirst: 13)".format(self.__name, self.__save_file), gp.__str__())
+        self.check_state(gp, 9, 1, "TEST_STATE", 21, 12, 13)
 
     def test_multiple_updates(self) -> None:
         gp = self.build_guinea_pig()
@@ -34,8 +37,7 @@ class TestGuineaPig(TestCase):
             self.assertFalse(gp.has_died())
             gp.update("TEST_STATE", [1, 2, 3], 1)
         self.assertFalse(gp.has_died())
-        self.assertEqual("GuineaPig:(Name: {0}, Save File: {1}, Lifespan: 9 days, Current Age: 27 mins, State: TEST_STATE, "
-                         "Tired: 47, Hunger: 64, Thirst: 91)".format(self.__name, self.__save_file), gp.__str__())
+        self.check_state(gp, 9, 27, "TEST_STATE", 47, 64, 91)
 
     def test_can_die(self) -> None:
         lifespan = 4
@@ -44,5 +46,4 @@ class TestGuineaPig(TestCase):
             self.assertFalse(gp.has_died())
             gp.update("TEST_STATE", [0, 0, 0], 1)
         self.assertTrue(gp.has_died())
-        self.assertEqual("GuineaPig:(Name: {0}, Save File: {1}, Lifespan: 4 days, Current Age: 5760 mins, State: TEST_STATE, "
-                         "Tired: 20, Hunger: 10, Thirst: 10)".format(self.__name, self.__save_file), gp.__str__())
+        self.check_state(gp, lifespan, 5760, "TEST_STATE", 20, 10, 10)
