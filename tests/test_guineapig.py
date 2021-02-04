0edc246ba8e243494d17a782d835fd8b3a3526ae
John Chase <ratteal@gmail.com>
2021-02-04 19:06:41 +0000

    Use a common pig to test.

--- tests/test_guineapig.py
+++ tests/test_guineapig.py
@@ -12,13 +12,16 @@ class TestGuineaPig(TestCase):
         self.__name = "TestGP"
         self.__save_file = build_save_filename(os.environ.get('HOME', ''), self.__name)
 
+    def build_guinea_pig(self, lifespan: int = 9) -> GuineaPig:
+        return GuineaPig(self.__name, self.__save_file, lifespan, 0, "SLEEPING", 20, 10, 10, Mock())
+
     def test_initial_str(self) -> None:
-        gp = GuineaPig(self.__name, self.__save_file, 9, 0, "SLEEPING", 20, 10, 10, Mock())
+        gp = self.build_guinea_pig()
         self.assertEqual("GuineaPig:(Name: {0}, Save File: {1}, Lifespan: 9 days, Current Age: 0 mins, State: SLEEPING, "
                          "Tired: 20, Hunger: 10, Thirst: 10)".format(self.__name, self.__save_file), gp.__str__())
 
     def test_single_update(self) -> None:
-        gp = GuineaPig(self.__name, self.__save_file, 9, 0, "SLEEPING", 20, 10, 10, Mock())
+        gp = self.build_guinea_pig()
         self.assertFalse(gp.has_died())
         gp.update("TEST_STATE", [1, 2, 3], 1)
         self.assertFalse(gp.has_died())
@@ -26,7 +29,7 @@ class TestGuineaPig(TestCase):
                          "Tired: 21, Hunger: 12, Thirst: 13)".format(self.__name, self.__save_file), gp.__str__())
 
     def test_multiple_updates(self) -> None:
-        gp = GuineaPig(self.__name, self.__save_file, 9, 0, "SLEEPING", 20, 10, 10, Mock())
+        gp = self.build_guinea_pig()
         for i in range(27):
             self.assertFalse(gp.has_died())
             gp.update("TEST_STATE", [1, 2, 3], 1)
@@ -36,7 +39,7 @@ class TestGuineaPig(TestCase):
 
     def test_can_die(self) -> None:
         lifespan = 4
-        gp = GuineaPig(self.__name, self.__save_file, lifespan, 0, "SLEEPING", 20, 10, 10, Mock())
+        gp = self.build_guinea_pig(lifespan)
         for i in range(lifespan * 24 * 60):
             self.assertFalse(gp.has_died())
             gp.update("TEST_STATE", [0, 0, 0], 1)
